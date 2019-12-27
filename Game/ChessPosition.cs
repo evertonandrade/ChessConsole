034f12be8f132a68864134f76ebc51cd99e22cbb
Everton Andrade <evertonfa7@gmail.com>
2019-12-27 18:58:55 -0300

    Criando a classe ChessPosition

---
+++ Game/ChessPosition.cs
@@ -0,0 +1,30 @@
+using ChessConsole.Board;
+
+namespace ChessConsole.Game
+{
+    public class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public ChessPosition()
+        {
+        }
+
+        public ChessPosition(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8- Line, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,11 +8,10 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            Chessboard tab = new Chessboard(8,8);
-            tab.InsertPiece(new King(tab, Color.Black), new Position(9, 0));
-            tab.InsertPiece(new Rook(tab, Color.Black), new Position(0, 3));
-            tab.InsertPiece(new Rook(tab, Color.Black), new Position(0, 0));
-            Screen.ShowBoard(tab);
+            ChessPosition pos = new ChessPosition('c', 7);
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.ToPosition());
         }
     }
 }
