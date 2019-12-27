ea7a78d981ddca2a2d6a85cf24db170625cc789f
Everton Andrade <evertonfa7@gmail.com>
2019-12-26 23:32:55 -0300

    Criando a classe Position

---
+++ Board/Position.cs
@@ -0,0 +1,23 @@
+namespace ChessConsole.Board
+{
+    public class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position()
+        {
+        }
+
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return $"({Line}, {Column})";
+        }
+    }
+}
